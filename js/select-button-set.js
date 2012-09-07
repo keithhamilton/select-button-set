61dfaf22a2c6a7381d715551d3946c7148901001
Keith Hamilton <the.keith.hamilton@gmail.com>
2012-09-07 08:20:20 -0700

    add multiple subscriber register/unregister
    converted to prototype pattern, added support for adding/removing multiple subscribers

--- js/select-button-set.js
+++ js/select-button-set.js
@@ -1,75 +1,86 @@
-var SelectButtonSet = function SelectButtonSet(context, event_trigger, event_target)
+var SelectButtonSet = function SelectButtonSet(context, event_trigger)
 {
-    var $context = context,
-     	event_trigger = (typeof event_trigger !== 'undefined') ? event_trigger : 'select-button-set-changed',
-        $target = (typeof event_target !== 'undefined') ? event_target : 'document',
-        $buttons;
+    this.$context,
+    this.targets = [],
+    this.$buttons,
+    this.event_trigger = (typeof event_trigger !== 'undefined') ? event_trigger : 'select-button-set-changed';
+    this._initialize(context);
+};
 
-	function _initialize()
+SelectButtonSet.prototype._initialize = function(context){
+	try
 	{
-		try
-		{
-			context.attr('class');
-		}
-		catch(err)
-		{
-			$context = $(context)
-		}
-
-		try
-		{
-			target.attr('class');
-		}
-		catch(err)
-		{
-			$target = $(target);
-		}
-
-		if(!$context[0])
-		{
-			return;
-		}
-
-		var buttons = $context.find('.select-button-set-option'); 
-
-		_drawButtons(buttons);
-
-	};
-
-	function _drawButtons(buttons)
+		context.attr('class');
+	}
+	catch(err)
 	{
-		$context.find('.select-button-set-select').hide();
-		for (var i=0;i<buttons.length;i++)
-		{
-		    $button = $("<div class='select-button-set-button'></div>");
-		    if($(buttons[i]).hasClass('select-button-set-option-default')){
-		        $button.addClass('select-button-set-button-selected');
-		    }
-			$button.attr('value',$(buttons[i]).val())
-					.text($(buttons[i]).text());
-			if(i==0){
-				$button.addClass('select-button-set-button-first');
-			}
-			else if (i==buttons.length-1)
-			{
-				$button.addClass('select-button-set-button-last');
-			}
-			$context.append($button);
-			$button.click(_onButtonClick);
-		}
+		$context = $(context)
+	}
 
+	var buttons = $context.find('.select-button-set-option'); 
+
+	this._drawButtons(buttons);
+
+};
+
+SelectButtonSet.prototype._drawButtons = function (buttons)
+{
+    $context.find('.select-button-set-select').hide();
+    for (var i=0;i<buttons.length;i++)
+    {
+        $button = $("<div class='select-button-set-button'></div>");
+        if($(buttons[i]).hasClass('select-button-set-option-default')){
+            $button.addClass('select-button-set-button-selected');
+        }
+        $button.attr('value',$(buttons[i]).val())
+                .text($(buttons[i]).text());
+        if(i==0){
+            $button.addClass('select-button-set-button-first');
+        }
+        else if (i==buttons.length-1)
+        {
+            $button.addClass('select-button-set-button-last');
+        }
+        $context.append($button);
+        $button.click($.proxy(this._onButtonClick, this));
+    }
+};
+
+SelectButtonSet.prototype._onButtonClick = function (event) {
+    $('.select-button-set-button-selected').removeClass('select-button-set-button-selected');
+    $(event.target).addClass('select-button-set-button-selected');
+    for (var i = 0; i < this.targets.length; i++) {
+        $(this.targets[i]).trigger(this.event_trigger, { value: $(event.target).val() });
+    }
+};
+
+
+SelectButtonSet.prototype.addSubscriber = function (subscriber) {
+    var selector,
+        self = this;
+    try {
+        selector = subscriber.attr('class')!=null ? '.'+subscriber.attr('class') : '#'+subscriber.attr('id');
         
+    }
+    catch (err) {
+        selector = subscriber;
+    }
 
-	}
+    self.targets.push(selector);
+};
 
-	function _onButtonClick(){
-		$('.select-button-set-button-selected').removeClass('select-button-set-button-selected');
-		$(this).addClass('select-button-set-button-selected');
-		$target.trigger(event_trigger,{value: $(this).val()});
-	}
+SelectButtonSet.prototype.removeSubscriber = function (subscriber) {
+    var selector;
+    
+    try {
+        selector = subscriber.attr('class') != null ? '.'+subscriber.attr('class') : '#'+subscriber.attr('id');
+    }
+    catch (err) {
+        selector = subscriber;
+    }
 
-	return{
-		initialize: _initialize
-	};
-
-}
+    var i = this.targets.indexOf(selector);
+    if (i != -1) {
+        this.targets.splice(i, 1);
+    }
+};
